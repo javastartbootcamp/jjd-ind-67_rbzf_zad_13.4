a59af128a63a81f3393ce6fc8049a0d1d830c8dd
rbzf <anna.sadkowska@gmail.com>
2023-04-28 06:29:24 +0200

    Rozwiazanie

--- src/main/java/pl/javastart/task/CustomList.java
+++ src/main/java/pl/javastart/task/CustomList.java
@@ -2,5 +2,66 @@ package pl.javastart.task;
 
 // Uzupełnij klasę. Możesz ją dowolnie modyfikować.
 // Celem jest, żeby przechodziły testy w src/test/java/[...]/CustomListTest
-public class CustomList {
+import java.util.Arrays;
+
+public class CustomList<T> {
+
+    private int size = 0;
+    private static final int INITIAL_CAPACITY = 2;
+    private Object[] elements;
+
+    public CustomList() {
+        elements = new Object[INITIAL_CAPACITY];
+    }
+
+    public void add(T element) {
+        if (size == elements.length) {
+            ensureCapacity();
+        }
+        elements[size] = element;
+        size++;
+    }
+
+    public void add(int index, T element) {
+        if (size == elements.length) {
+            ensureCapacity();
+        }
+        if (index >= 0 && index < size) {
+            int numberOfElementsToBeCopied = elements.length - (index + 1);
+            System.arraycopy(elements, index, elements, index + 1, numberOfElementsToBeCopied);
+            elements[index] = element;
+            size++;
+        } else if (index == size) {
+            add(element);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public T get(int index) {
+        if (index >= 0 && index <= size) {
+            return (T) elements[index];
+        }
+        throw new ArrayIndexOutOfBoundsException("niewlasciwy index elementu");
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void remove(int index) {
+        int numberOfElementsToBeCopied = elements.length - (index + 1);
+        System.arraycopy(elements, index + 1, elements, index, numberOfElementsToBeCopied);
+        size--;
+    }
+
+    private void ensureCapacity() {
+        elements = Arrays.copyOf(elements, size * 2);
+    }
+
+    @Override
+    public String toString() {
+        elements = Arrays.copyOf(elements, size);
+        return Arrays.toString(elements);
+    }
 }
+
--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -1,5 +1,8 @@
 package pl.javastart.task;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,8 +10,33 @@ public class Main {
         // tutaj możesz przeprowadzać manualne testy listy
 
         // lista powinna być parametryzowana (analogicznie jak java.util.ArrayList czy java.util.LinkedList)
-        CustomList<String> customListOfStrings = new CustomList();
-        CustomList<Integer> customListOfIntegers = new CustomList();
+        CustomList<String> customListOfStrings = new CustomList<>();
+        CustomList<Integer> customListOfIntegers = new CustomList<>();
 
+        customListOfStrings.add("Ania");
+        customListOfStrings.add("Kasia");
+        customListOfStrings.add("Wojtek");
+        customListOfStrings.add("Tomek");
+
+        System.out.println(customListOfStrings);
+        System.out.println(customListOfStrings.get(2));
+        System.out.println(customListOfStrings.size());
+        customListOfStrings.add(3, "Iza");
+        System.out.println(customListOfStrings);
+        customListOfStrings.remove(0);
+        System.out.println(customListOfStrings);
+
+        customListOfIntegers.add(1);
+        customListOfIntegers.add(2);
+        customListOfIntegers.add(3);
+        customListOfIntegers.add(4);
+
+        System.out.println(customListOfIntegers);
+        System.out.println(customListOfIntegers.get(2));
+        System.out.println(customListOfIntegers.size());
+        customListOfIntegers.add(3, 10);
+        System.out.println(customListOfIntegers);
+        customListOfIntegers.remove(0);
+        System.out.println(customListOfIntegers);
     }
 }
